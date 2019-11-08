613555c3f8ee54c70ab8886048935fd5800f0459
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2019-11-08 17:17:00 -0300

    Adição do gerador de artefato

---
+++ gerador-artefato.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ./gerador-artefato.sh -d /kdi/git/crm-patrimonio-api -u c1282036 -t 900089
+
+while getopts "d:u:t:" opt
+do
+   case "$opt" in
+      d ) DIRETORIO_PROJETO="$OPTARG" ;;
+      u ) USUARIO="$OPTARG" ;;
+      t ) TASK="$OPTARG" ;;
+   esac
+done
+
+gerarArtefato()
+{
+    if [ -d $1 ]; then
+        git -C $1 log --author=$2 --all --name-status -C --grep=$3| tac | grep -E "^[A,M,C,D]\s|^R.*\s" | sort -u -k 2 | awk -F '\t' '{print $1 "\t'"$(basename $1)"'/" $2 }'
+    fi
+}
+
+gerarArtefato $DIRETORIO_PROJETO $USUARIO $TASK
