3424acd4f733db9519004b2cd4729883d3d90dd1
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2019-12-14 10:17:33 -0300

    Update

--- init.js
+++ init.js
@@ -2,14 +2,10 @@
 
 const args = process.argv.slice(2)
 const Param = require('./models/param')
-const debug = require('debug')('gerador-artefato:server');
-
-const PORT = '3000'
-let server = {}
 
 init(args)
 
-function init(args) {
+async function init(args) {
 
     const params = Param.getFromArgs(args)
 
@@ -24,7 +20,8 @@ function init(args) {
 
         const gerador = require('./lib/gerador')(params)
 
-        // await gerador.gerarListaArtefato()
-        // console.log(params)
+        await gerador.gerarListaArtefato()
+        
+        console.log(params)
     }
 }
