a6e40c3b087e670281cddb92beeae9bfa9ba34ec
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2019-12-14 11:36:07 -0300

    Update

--- config/commander.js
+++ config/commander.js
@@ -5,12 +5,12 @@ const package = require('../package.json')
 program
     .description('Comando para listar os artefatos incluídos/alterados/renomeados para geração do QAS')
     .option('-s, --server', 'inicia a versão server')
-    .option('-d, --diretorio <type>', 'foo')
-    .option('-p, --projeto <type>', 'foo', commaSeparatedList)
-    .option('-a, --autor <type>', 'foo')
-    .option('-t, --task <type>', 'foo', commaSeparatedList)
-    .option('--mostrar-num-modificacao', 'foo')
-    .option('--mostrar-deletados', 'foo')
+    .option('-d, --diretorio <type>', 'diretório raiz dos projetos Git')
+    .option('-p, --projeto <type>', 'lista de projetos Git (podem ser passados vários projetos separados por vírgula)', commaSeparatedList)
+    .option('-a, --autor <type>', 'matrícula do autor dos commits')
+    .option('-t, --task <type>', 'lista de tarefas (podem ser passadas várias tarefas separadas por vírgula)', commaSeparatedList)
+    .option('--mostrar-num-modificacao', 'nº de modificações do artefato na tarefa ou tarefas')
+    .option('--mostrar-deletados', 'mostrar artefatos deletados na tarefa')
     .version(package.version, '-v, --version', 'mostra a versão do programa')
 
 function commaSeparatedList(value, dummyPrevious) {
