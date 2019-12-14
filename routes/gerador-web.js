8c604d00b21c7f0cc88094266cf6155a6b310325
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2019-12-14 11:59:05 -0300

    Update

--- routes/gerador-web.js
+++ routes/gerador-web.js
@@ -3,10 +3,18 @@ module.exports = function (app) {
 
     app.post('/gerador', async function (req, resp) {
 
-        const Param = require('../models/param')
-        const params = new Param(req.body)
-        const gerador = require('../lib/gerador')(params)
+        const BAD_REQUEST_CODE = 400
 
-        resp.json(await gerador.gerarListaArtefato())
+        try {
+            const Param = require('../models/param')
+            const params = new Param(req.body)
+            const gerador = require('../lib/gerador')(params)
+
+            resp.json(await gerador.gerarListaArtefato())
+
+        } catch (error) {
+
+            resp.status(BAD_REQUEST_CODE).send({ message: error.message })
+        }
     })
 }
