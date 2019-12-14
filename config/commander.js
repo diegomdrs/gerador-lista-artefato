42ce806efb75ba04a747c00ac43abda440f8b898
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2019-12-14 12:26:39 -0300

    Update

--- config/commander.js
+++ config/commander.js
@@ -4,14 +4,14 @@ const package = require('../package.json')
 
 program
     .description('Comando para listar os artefatos incluídos/alterados/renomeados para geração do QAS')
-    .option('-s, --server', 'inicia a versão server e ignora os outros parâmetros')
-    .option('-d, --diretorio <type>', 'diretório raiz dos projetos Git')
-    .option('-p, --projeto <type>', 'lista de projetos Git (podem ser passados vários projetos separados por vírgula)', commaSeparatedList)
-    .option('-a, --autor <type>', 'matrícula do autor dos commits')
-    .option('-t, --task <type>', 'lista de tarefas (podem ser passadas várias tarefas separadas por vírgula)', commaSeparatedList)
-    .option('--mostrar-num-modificacao', 'nº de modificações do artefato na tarefa ou tarefas')
-    .option('--mostrar-deletados', 'mostrar artefatos deletados na tarefa')
-    .version(package.version, '-v, --version', 'mostra a versão do programa')
+    .option('-s, --server', 'Inicia a versão server e ignora os outros parâmetros')
+    .option('-d, --diretorio <type>', 'Diretório raiz dos projetos Git')
+    .option('-p, --projeto <type>', 'Lista de projetos Git (podem ser passados vários projetos separados por vírgula)', commaSeparatedList)
+    .option('-a, --autor <type>', 'Matrícula do autor dos commits')
+    .option('-t, --task <type>', 'Lista de tarefas (podem ser passadas várias tarefas separadas por vírgula)', commaSeparatedList)
+    .option('--mostrar-num-modificacao', 'Nº de modificações do artefato na tarefa ou tarefas')
+    .option('--mostrar-deletados', 'Mostra artefatos deletados na tarefa')
+    .version(package.version, '-v, --version', 'Mostra a versão do programa')
 
 function commaSeparatedList(value, dummyPrevious) {
     return value.split(',');
