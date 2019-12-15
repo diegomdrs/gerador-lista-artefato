0d906fb79fc4d9adb05003622df0cf8a60275d27
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2019-12-15 10:15:42 -0300

    Update

--- test/gerador.test.js
+++ test/gerador.test.js
@@ -1,6 +1,28 @@
 const gerador = require('../lib/gerador')
+const exec = require('child_process').exec
 
-test('test foo', () => {
-    
-    gerador({autor: 'diegomdrs'})
-});
+function execInTestDir(command, cb) {
+    exec(command, { cwd: __dirname }, cb)
+}
+
+describe('test foo', () => {
+
+    beforeEach(function (done) {
+        execInTestDir(__dirname + '/delete-repo.sh', function (error) {
+            if (error) {
+                return done(error)
+            }
+            execInTestDir(__dirname + '/create-repo.sh', done)
+        })
+    })
+
+    it('test one', () => { 
+        gerador({autor: 'diegomdrs'})
+    })
+
+    afterEach((done) => {
+        execInTestDir(__dirname + '/delete-repo.sh', function () {
+            done()
+        })
+    })
+})
