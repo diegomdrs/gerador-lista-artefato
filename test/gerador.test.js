56e0468640d4557375fe9c14c606a672119345d0
Diego Medeiros <c1282036@bb.com.br>
2019-12-16 19:05:11 -0300

    Update

--- test/gerador.test.js
+++ test/gerador.test.js
@@ -5,7 +5,7 @@ const app = require('../package.json')
 const crypto = require('crypto')
 
 const NAME_APP = app.name
-const PATH_TEST = '/tmp' + '/' + NAME_APP
+const PATH_TEST = '/tmp/' + NAME_APP
 
 let git = {}
 
@@ -13,69 +13,77 @@ describe('test foo', () => {
 
     beforeEach(async () => {
 
-        if (fs.pathExistsSync(PATH_TEST)) {
-            fs.removeSync(PATH_TEST)
-        }
+        // if (fs.pathExistsSync(PATH_TEST)) {
+        //     fs.removeSync(PATH_TEST)
+        // }
 
-        fs.mkdirSync(PATH_TEST)
+        await createRepo('/apc-estatico')
+        await createRepo('/crm-patrimonio-estatico')
 
-        git = require('simple-git/promise')(PATH_TEST)
-        await git.init()
+        createFile('/crm-patrimonio-estatico/src/app/spas/imovel/documentos',
+            'lista-documentos.tpl.html')
+        commitFile('0000000', '/crm-patrimonio-estatico/src/app/spas/imovel/documentos',
+            'lista-documentos.tpl.html')
 
-        // Criação
-        fs.outputFileSync(PATH_TEST + '/arquivo1.txt')
-        await git.add('./arquivo1.txt')
-        await git.commit("task 1111111 commit")
+        // modifieFile('/crm-patrimonio-estatico/src/app/spas/imovel/documentos',
+        //     'lista-documentos.tpl.html')
+        // commitFile('0000000', '/crm-patrimonio-estatico/src/app/spas/imovel/documentos',
+        //     'lista-documentos.tpl.html')
 
-        // Criação
-        fs.outputFileSync(PATH_TEST + '/arquivo2.txt')
-        await git.add('./arquivo2.txt')
-        await git.commit("task 2222222 commit")
+        // // Criação
+        // fs.outputFileSync(PATH_TEST + '/arquivo1.txt')
+        // await git.add('./arquivo1.txt')
+        // await git.commit("task 1111111 commit")
 
-        // Criação
-        fs.outputFileSync(PATH_TEST + '/arquivo3.txt')
-        await git.add('./arquivo3.txt')
-        await git.commit("task 2222222 commit")
+        // // Criação
+        // fs.outputFileSync(PATH_TEST + '/arquivo2.txt')
+        // await git.add('./arquivo2.txt')
+        // await git.commit("task 2222222 commit")
 
-        // Modificação
-        fs.outputFileSync(PATH_TEST + '/arquivo1.txt', randomValueHex(12))
-        await git.add('./arquivo1.txt')
-        await git.commit("task 1111111 commit")
+        // // Criação
+        // fs.outputFileSync(PATH_TEST + '/arquivo3.txt')
+        // await git.add('./arquivo3.txt')
+        // await git.commit("task 2222222 commit")
 
-        // Modificação
-        fs.outputFileSync(PATH_TEST + '/arquivo1.txt', randomValueHex(12))
-        await git.add('./arquivo1.txt')
-        await git.commit("task 2222222 commit")
+        // // Modificação
+        // fs.outputFileSync(PATH_TEST + '/arquivo1.txt', randomValueHex(12))
+        // await git.add('./arquivo1.txt')
+        // await git.commit("task 1111111 commit")
 
-        // Modificação
-        fs.outputFileSync(PATH_TEST + '/arquivo1.txt', randomValueHex(12))
-        await git.add('./arquivo1.txt')
-        await git.commit("task 2222222 commit")
+        // // Modificação
+        // fs.outputFileSync(PATH_TEST + '/arquivo1.txt', randomValueHex(12))
+        // await git.add('./arquivo1.txt')
+        // await git.commit("task 2222222 commit")
+
+        // // Modificação
+        // fs.outputFileSync(PATH_TEST + '/arquivo1.txt', randomValueHex(12))
+        // await git.add('./arquivo1.txt')
+        // await git.commit("task 2222222 commit")
     })
 
     it('test one', async () => {
 
-        const params = new Param({
-            diretorio: "/tmp",
-            autor: "diegomdrs",
-            projeto: NAME_APP,
-            task: ["1111111", "2222222"]
-        })
+        // const params = new Param({
+        //     diretorio: "/tmp",
+        //     autor: "diegomdrs",
+        //     projeto: NAME_APP,
+        //     task: ["1111111", "2222222"]
+        // })
 
-        const retorno = await gerador(params).gerarListaArtefato()
+        // const retorno = await gerador(params).gerarListaArtefato()
 
-        console.log('listaArtefatoTarefaMesmoTipo ================================================')
-        for (const artefato of retorno.listaArtefato) {
-            console.log(artefato.nomeArtefato + ' ##########################')
-            console.table(artefato.listaTarefa)
-        }
+        // console.log('listaArtefatoTarefaMesmoTipo ================================================')
+        // for (const artefato of retorno.listaArtefato) {
+        //     console.log(artefato.nomeArtefato + ' ##########################')
+        //     console.table(artefato.listaTarefa)
+        // }
 
-        console.log('listaArtefatoTarefasIguais   ================================================')
-        for (const tarefaFoo of retorno.listaTarefaFoo) {
-            console.table(tarefaFoo.listaArtefatoFoo)
-        }
+        // console.log('listaArtefatoTarefasIguais   ================================================')
+        // for (const tarefaFoo of retorno.listaTarefaFoo) {
+        //     console.table(tarefaFoo.listaArtefatoFoo)
+        // }
 
-        expect(retorno).toBeDefined()
+        // expect(retorno).toBeDefined()
     })
 
     afterEach(() => {
@@ -89,4 +97,29 @@ function randomValueHex(len) {
         .randomBytes(Math.ceil(len / 2))
         .toString('hex')
         .slice(0, len)
+}
+
+async function createRepo(path) {
+
+    fs.mkdirsSync(PATH_TEST + path)
+
+    git = require('simple-git/promise')(PATH_TEST + path)
+    await git.init()
+}
+
+async function createFile(path, fileName) {
+
+    fs.mkdirsSync(PATH_TEST + path)
+    fs.outputFileSync(PATH_TEST + path + '/' + fileName)
+}
+
+async function modifieFile(path, fileName) {
+
+    fs.outputFileSync(PATH_TEST + path + '/' + fileName, randomValueHex(12))
+}
+
+async function commitFile(task, path, fileName) {
+
+    await git.add(PATH_TEST + path + '/' + fileName)
+    await git.commit('task ' + task + ' commit')
 }
