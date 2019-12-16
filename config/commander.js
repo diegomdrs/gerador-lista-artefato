8eeea09063c41d79a63b8e654edf0ae7e42c2344
Diego Medeiros <c1282036@bb.com.br>
2019-12-16 17:52:30 -0300

    Update

--- config/commander.js
+++ config/commander.js
@@ -4,13 +4,13 @@ const package = require('../package.json')
 
 program
     .description('Comando para listar os artefatos incluídos/alterados/renomeados para geração do QAS')
-    .option('-s, --server', 'Inicia a versão server e ignora os outros parâmetros')
     .option('-d, --diretorio <type>', 'Diretório raiz dos projetos Git')
     .option('-p, --projeto <type>', 'Lista de projetos Git (podem ser passados vários projetos separados por vírgula)', commaSeparatedList)
     .option('-a, --autor <type>', 'Matrícula do autor dos commits')
     .option('-t, --task <type>', 'Lista de tarefas (podem ser passadas várias tarefas separadas por vírgula)', commaSeparatedList)
     .option('--mostrar-num-modificacao', 'Nº de modificações do artefato na tarefa ou tarefas (Opcional)')
     .option('--mostrar-deletados', 'Mostra artefatos deletados na tarefa (Opcional)')
+    .option('-s, --server', 'Inicia a versão server e ignora os outros parâmetros')
     .version(package.version, '-v, --version', 'Mostra a versão do programa')
 
 function commaSeparatedList(value, dummyPrevious) {
