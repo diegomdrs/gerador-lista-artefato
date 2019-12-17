beb6ae292b922feebc7732b1d46322a390357ec6
Diego Medeiros <c1282036@bb.com.br>
2019-12-17 16:47:30 -0300

    Update

--- test/gerador.test.js
+++ test/gerador.test.js
@@ -15,37 +15,40 @@ describe('test foo', () => {
 
     it('test one', async () => {
 
-        const repoApcEstatico = await createRepo('/apc-estatico')
-        const repoCrmPatrimonioEstatico = await createRepo('/crm-patrimonio-estatico')
+        const listaFoo = [
+            {
+                repo: {},
+                nomeProjeto: 'apc-estatico',
+                listaArtefato: [{
+                    pathArtefato: 'apc-estatico',
+                    nomeArtefato: 'package.json',
+                    listaTarefa: [
+                        { numTarefa: '0000000', numAlteracao: 1, tipoAlteracao: 'A' },
+                        { numTarefa: '1207175', numAlteracao: 1, tipoAlteracao: 'M' },
+                        { numTarefa: '1212444', numAlteracao: 1, tipoAlteracao: 'M' }
+                    ]
+                }]
+            }
+        ]
 
-        //  apc-estatico/package.json
-        await createFile(repoApcEstatico,  '0000000', '/apc-estatico', 'package.json')
-        await modifieFile(repoApcEstatico, '1207175', '/apc-estatico', 'package.json')
-        await modifieFile(repoApcEstatico, '1212444', '/apc-estatico', 'package.json')
+        for (const foo of listaFoo) {
 
-        // crm-patrimonio-estatico/src/app/spas/imovel/documentos/lista-documentos.tpl.html
-        await createFile(repoCrmPatrimonioEstatico,  '000000',
-            '/crm-patrimonio-estatico/src/app/spas/imovel/documentos', 'lista-documentos.tpl.html')
-        await modifieFile(repoCrmPatrimonioEstatico, '1203670',
-            '/crm-patrimonio-estatico/src/app/spas/imovel/documentos', 'lista-documentos.tpl.html')
-        await modifieFile(repoCrmPatrimonioEstatico, '1210684',
-            '/crm-patrimonio-estatico/src/app/spas/imovel/documentos', 'lista-documentos.tpl.html')
+            foo.repo = await createRepo(foo.nomeProjeto)
 
-        // crm-patrimonio-estatico/package.json  
-        await createFile( repoCrmPatrimonioEstatico, '0000000', '/crm-patrimonio-estatico', 'package.json')
-        await modifieFile(repoCrmPatrimonioEstatico, '1199211', '/crm-patrimonio-estatico', 'package.json')
-        await modifieFile(repoCrmPatrimonioEstatico, '1199211', '/crm-patrimonio-estatico', 'package.json')
-        await modifieFile(repoCrmPatrimonioEstatico, '1203082', '/crm-patrimonio-estatico', 'package.json')
-        await modifieFile(repoCrmPatrimonioEstatico, '1203082', '/crm-patrimonio-estatico', 'package.json')
-        await modifieFile(repoCrmPatrimonioEstatico, '1203670', '/crm-patrimonio-estatico', 'package.json')
-        await modifieFile(repoCrmPatrimonioEstatico, '1203670', '/crm-patrimonio-estatico', 'package.json')
-        await modifieFile(repoCrmPatrimonioEstatico, '1207175', '/crm-patrimonio-estatico', 'package.json')
-        await modifieFile(repoCrmPatrimonioEstatico, '1210684', '/crm-patrimonio-estatico', 'package.json')
+            for (const artefato of foo.listaArtefato) {
+
+                for (const tarefa of artefato.listaTarefa) {
+
+                    await fooFile(foo.repo, tarefa.tipoAlteracao, tarefa.numTarefa, 
+                        artefato.pathArtefato, artefato.nomeArtefato)
+                }
+            }
+        }
 
         const params = new Param({
             diretorio: PATH_TEST,
             autor: "fulano",
-            projeto: ["apc-estatico", "crm-patrimonio-estatico"],
+            projeto: ["apc-estatico"],
             task: [1199211, 1203082, 1203670, 1207175, 1210684, 1210658, 1212262, 1212444]
         })
 
@@ -54,14 +57,6 @@ describe('test foo', () => {
         expect(lista[0].listaNumTarefa).toHaveLength(2)
         expect(lista[0].listaArtefatoFoo[0].numeroAlteracao).toBe(2)
         expect(lista[0].listaArtefatoFoo[0].tipoAlteracao).toBe('M')
-
-        expect(lista[1].listaNumTarefa).toHaveLength(2)
-        expect(lista[1].listaArtefatoFoo[0].numeroAlteracao).toBe(2)
-        expect(lista[1].listaArtefatoFoo[0].tipoAlteracao).toBe('M')
-
-        expect(lista[2].listaNumTarefa).toHaveLength(5)
-        expect(lista[2].listaArtefatoFoo[0].numeroAlteracao).toBe(8)
-        expect(lista[2].listaArtefatoFoo[0].tipoAlteracao).toBe('M')
     })
 
     afterEach(() => {
@@ -78,15 +73,24 @@ function randomValueHex(len) {
 
 async function createRepo(path) {
 
-    fs.mkdirsSync(PATH_TEST + path)
+    fs.mkdirsSync(PATH_TEST + '/' + path)
 
-    const git = require('simple-git/promise')(PATH_TEST + path)
+    const git = require('simple-git/promise')(PATH_TEST + '/' + path)
 
     await git.init()
     await git.addConfig('user.name', 'fulano')
     await git.addConfig('user.email', 'fulano@fulano.com')
 
     return git
+}
+
+
+async function fooFile(git, tipoAlteracao, task, path, fileName) {
+
+    tipoAlteracao === 'A' && fs.mkdirsSync(PATH_TEST + '/' + path)
+    fs.writeFileSync(PATH_TEST + '/' +  path + '/' + fileName, randomValueHex(12))
+
+    await commitFile(git, task, path, fileName)
 }
 
 async function createFile(git, task, path, fileName) {
@@ -106,6 +110,6 @@ async function modifieFile(git, task, path, fileName) {
 
 async function commitFile(git, task, path, fileName) {
 
-    await git.add(PATH_TEST + path + '/' + fileName)
+    await git.add(PATH_TEST + '/' + path + '/' + fileName)
     await git.commit('task ' + task + ' commit')
 }
