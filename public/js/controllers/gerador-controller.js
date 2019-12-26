b2867f99c1ec3354821a7f0b2919cfe0d5f02352
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2019-12-26 11:16:06 -0300

    Update

--- public/js/controllers/gerador-controller.js
+++ public/js/controllers/gerador-controller.js
@@ -8,20 +8,21 @@ function GeradorController(geradorService) {
     var vm = this
 
     vm.listaSaida = []
+    vm.req = {}
 
     vm.init = init
     vm.obterNumero = obterNumero
 
+    vm.req = {
+        diretorio: "/home/foo/Documents/gerador-lista-artefato-qas/test/gerador-lista-artefato-qas",
+        autor: "fulano",
+        projeto: ["apc-estatico", "apc-api", "crm-patrimonio-estatico", "crm-patrimonio-api"],
+        task: ["1168815", "1172414", "1168800", "1167319", "1163642", "1155478", "1150152", "1161422"]
+    }
+
     function init() {
 
-        const req = {
-            diretorio: "/home/foo/Documents/gerador-lista-artefato-qas/test/gerador-lista-artefato-qas",
-            autor: "fulano",
-            projeto: ["apc-estatico", "apc-api", "crm-patrimonio-estatico", "crm-patrimonio-api"],
-            task: ["1168815", "1172414", "1168800", "1167319", "1163642", "1155478", "1150152", "1161422"]
-        }
-
-        geradorService.gerarListaArtefato(req)
+        geradorService.gerarListaArtefato(vm.req)
             .then(function (resposta) {
                 vm.listaSaida = resposta.data
             }, function (error) {
