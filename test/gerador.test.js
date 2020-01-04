f4dd1473541636d59503d8aa39ca8f998239ad37
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2020-01-04 20:59:26 -0300

    Update

--- test/gerador.test.js
+++ test/gerador.test.js
@@ -12,6 +12,7 @@ describe('test gerais', () => {
 
         git = await geradorUtilTest.criarRepo(nomeProjeto)
         gerador = require('../lib/gerador')
+
         params = new Param({
             autor: "fulano",
             projeto: [
@@ -23,7 +24,7 @@ describe('test gerais', () => {
         })
     })
 
-    xit('test parâmetros inválidos', () => {
+    it('test parâmetros inválidos', () => {
 
         // const req = {
         //     diretorio: "/home/foo/Documents/gerador-lista-artefato-qas/test/gerador-lista-artefato-qas",
@@ -33,7 +34,7 @@ describe('test gerais', () => {
         // }
     });
 
-    ('test listagem de artefatos com projeto inválido', () => {
+    ('teste de listagem de artefatos com projeto inválido', () => {
 
         const paramsError = new Param({
             autor: "fulano",
@@ -46,7 +47,7 @@ describe('test gerais', () => {
             new Error('Projeto \'' + paramsError.projeto[0] + '\' não encontrado'));
     });
 
-    xit('test listagem de artefatos renomeados', async () => {
+    it('teste de listagem de artefatos renomeados', async () => {
 
         await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
             'arquivoFoo.txt', 'A')
@@ -63,25 +64,29 @@ describe('test gerais', () => {
 
         const lista = await gerador(params).gerarListaArtefato()
 
+        expect(lista).toHaveLength(1)
+
         expect(lista[0].listaNumTarefaSaida).toHaveLength(1)
         expect(lista[0].listaNumTarefaSaida[0]).toBe('1111111')
 
+        expect(lista[0].listaArtefatoSaida).toHaveLength(3)
+
         expect(lista[0].listaArtefatoSaida[0].tipoAlteracao).toBe('A')
-        expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoQux.txt')
         expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
+        expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoQux.txt')
 
         expect(lista[0].listaArtefatoSaida[1].tipoAlteracao).toBe('M')
-        expect(lista[0].listaArtefatoSaida[1].nomeArtefato).toBe('foo/arquivoQux.txt')
         expect(lista[0].listaArtefatoSaida[1].numeroAlteracao).toBe(2)
+        expect(lista[0].listaArtefatoSaida[1].nomeArtefato).toBe('foo/arquivoQux.txt')
 
         expect(lista[0].listaArtefatoSaida[2].tipoAlteracao).toBe('R')
+        expect(lista[0].listaArtefatoSaida[2].numeroAlteracao).toBe(1)
         expect(lista[0].listaArtefatoSaida[2].nomeArtefato).toBe('foo/arquivoQux.txt')
         expect(lista[0].listaArtefatoSaida[2].nomeAntigoArtefato).toBe('foo/arquivoFoo.txt')
         expect(lista[0].listaArtefatoSaida[2].nomeNovoArtefato).toBe('foo/arquivoQux.txt')
-        expect(lista[0].listaArtefatoSaida[2].numeroAlteracao).toBe(1)
     })
 
-    xit('test listagem de artefatos renomeados 2 vezes', async () => {
+    it('teste de listagem de artefatos renomeados 2 vezes', async () => {
 
         await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
             'arquivoFoo.txt', 'A')
@@ -104,26 +109,30 @@ describe('test gerais', () => {
             'arquivoBar.txt', 'M')
 
         const lista = await gerador(params).gerarListaArtefato()
+
+        expect(lista).toHaveLength(1)
 
         expect(lista[0].listaNumTarefaSaida).toHaveLength(1)
         expect(lista[0].listaNumTarefaSaida[0]).toBe('1111111')
 
+        expect(lista[0].listaArtefatoSaida).toHaveLength(3)
+
         expect(lista[0].listaArtefatoSaida[0].tipoAlteracao).toBe('A')
-        expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoBar.txt')
         expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
+        expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoBar.txt')
 
         expect(lista[0].listaArtefatoSaida[1].tipoAlteracao).toBe('M')
-        expect(lista[0].listaArtefatoSaida[1].nomeArtefato).toBe('foo/arquivoBar.txt')
         expect(lista[0].listaArtefatoSaida[1].numeroAlteracao).toBe(3)
+        expect(lista[0].listaArtefatoSaida[1].nomeArtefato).toBe('foo/arquivoBar.txt')
 
         expect(lista[0].listaArtefatoSaida[2].tipoAlteracao).toBe('R')
+        expect(lista[0].listaArtefatoSaida[2].numeroAlteracao).toBe(2)
         expect(lista[0].listaArtefatoSaida[2].nomeArtefato).toBe('foo/arquivoBar.txt')
         expect(lista[0].listaArtefatoSaida[2].nomeAntigoArtefato).toBe('foo/arquivoQux.txt')
         expect(lista[0].listaArtefatoSaida[2].nomeNovoArtefato).toBe('foo/arquivoBar.txt')
-        expect(lista[0].listaArtefatoSaida[2].numeroAlteracao).toBe(2)
     })
 
-    xit('test listagem de artefato renomeado, deletado e criado novamente', async () => {
+    it('teste de listagem de artefato A, R, D e A novamente', async () => {
 
         await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
             'arquivoFoo.txt', 'A')
@@ -153,18 +162,23 @@ describe('test gerais', () => {
 
         const lista = await gerador(params).gerarListaArtefato()
 
-        expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
+        expect(lista).toHaveLength(1)
+
+        expect(lista[0].listaNumTarefaSaida).toHaveLength(1)
+        expect(lista[0].listaNumTarefaSaida[0]).toBe('1111111')
+
+        expect(lista[0].listaArtefatoSaida).toHaveLength(2)
+
         expect(lista[0].listaArtefatoSaida[0].tipoAlteracao).toBe('D')
+        expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
         expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoBar.txt')
 
-        expect(lista[0].listaArtefatoSaida[1].numeroAlteracao).toBe(1)
         expect(lista[0].listaArtefatoSaida[1].tipoAlteracao).toBe('A')
+        expect(lista[0].listaArtefatoSaida[1].numeroAlteracao).toBe(1)
         expect(lista[0].listaArtefatoSaida[1].nomeArtefato).toBe('foo/arquivoBar.txt')
     })
 
-    it('test ', async () => {
-
-        // node_modules/jest/bin/jest.js --runInBand --verbose test/gerador.test.js ; node app --diretorio=/tmp/gerador-lista-artefato-qas --projeto=foo --autor=fulano --task=1111111 --mostrar-num-modificacao --mostrar-deletados
+    it('teste de listagem de artefato A, M, D e A com mesmo nome, COM opção de mostrar deletados', async () => {
 
         await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
             'arquivoBar.txt', 'A')
@@ -177,29 +191,125 @@ describe('test gerais', () => {
 
         await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
             'arquivoBar.txt', 'A')
+
+        const lista = await gerador(params).gerarListaArtefato()
+
+        expect(lista).toHaveLength(1)
+
+        expect(lista[0].listaNumTarefaSaida).toHaveLength(1)
+        expect(lista[0].listaNumTarefaSaida[0]).toBe('1111111')
+
+        expect(lista[0].listaArtefatoSaida).toHaveLength(2)
+
+        expect(lista[0].listaArtefatoSaida[0].tipoAlteracao).toBe('D')
+        expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
+        expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoBar.txt')
+
+        expect(lista[0].listaArtefatoSaida[1].tipoAlteracao).toBe('A')
+        expect(lista[0].listaArtefatoSaida[1].numeroAlteracao).toBe(1)
+        expect(lista[0].listaArtefatoSaida[1].nomeArtefato).toBe('foo/arquivoBar.txt')
     })
 
-    xit('test listagem de artefatos commitados em branches diferentes', async () => {
+    it('teste de listagem de artefato A, M, D e A com mesmo nome, SEM opção de mostrar deletados', async () => {
+
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
+            'arquivoBar.txt', 'A')
+
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
+            'arquivoBar.txt', 'M')
+
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
+            'arquivoBar.txt', 'D')
+
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
+            'arquivoBar.txt', 'A')
+
+        params.mostrarDeletados = false
+
+        const lista = await gerador(params).gerarListaArtefato()
+
+        expect(lista).toHaveLength(1)
+
+        expect(lista[0].listaNumTarefaSaida).toHaveLength(1)
+expect(lista[0].listaNumTarefaSaida[0]).toBe('1111111')
+
+        expect(lista[0].listaArtefatoSaida).toHaveLength(1)
+
+        expect(lista[0].listaArtefatoSaida[0].tipoAlteracao).toBe('A')
+        expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
+        expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoBar.txt')
+    })
+
+    it('teste de listagem de artefato A, M, D COM opção de mostrar deletados', async () => {
+
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
+            'arquivoBar.txt', 'A')
+
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
+            'arquivoBar.txt', 'M')
+
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
+            'arquivoBar.txt', 'D')
+
+        const lista = await gerador(params).gerarListaArtefato()
+
+        expect(lista).toHaveLength(1)
+
+        expect(lista[0].listaNumTarefaSaida).toHaveLength(1)
+expect(lista[0].listaNumTarefaSaida[0]).toBe('1111111')
+
+        expect(lista[0].listaArtefatoSaida).toHaveLength(1)
+
+        expect(lista[0].listaArtefatoSaida[0].tipoAlteracao).toBe('D')
+        expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
+        expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoBar.txt')
+    })
+
+    it('teste de listagem de artefato A, M, D SEM opção de mostrar deletados', async () => {
+
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
+            'arquivoBar.txt', 'A')
+
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
+            'arquivoBar.txt', 'M')
+
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111',
+            'arquivoBar.txt', 'D')
+
+        params.mostrarDeletados = false
+
+        const lista = await gerador(params).gerarListaArtefato()
+
+        expect(lista).toHaveLength(0)
+    })
+
+    it('teste de listagem de artefatos criados em branches diferentes', async () => {
 
         await geradorUtilTest.checkoutBranch(git, 'branchFoo')
-        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111', 'arquivoFoo.txt', 'A')
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto,
+            '1111111', 'arquivoFoo.txt', 'A')
 
         await geradorUtilTest.checkoutBranch(git, 'branchBar')
-        await geradorUtilTest.manipularArquivo(git, nomeProjeto, '1111111', 'arquivoBar.txt', 'A')
+        await geradorUtilTest.manipularArquivo(git, nomeProjeto,
+            '1111111', 'arquivoBar.txt', 'A')
 
         await geradorUtilTest.checkoutBranch(git, 'master')
 
         const lista = await gerador(params).gerarListaArtefato()
 
-        expect(lista[0].listaNumTarefaSaida).toHaveLength(1)
-        expect(lista[0].listaNumTarefaSaida).toEqual(expect.arrayContaining(['1111111']))
+        expect(lista).toHaveLength(1)
 
-        expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
+        expect(lista[0].listaNumTarefaSaida).toHaveLength(1)
+expect(lista[0].listaNumTarefaSaida[0]).toBe('1111111')
+
+        expect(lista[0].listaArtefatoSaida).toHaveLength(2)
+
         expect(lista[0].listaArtefatoSaida[0].tipoAlteracao).toBe('A')
+        expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
         expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoFoo.txt')
 
-        expect(lista[0].listaArtefatoSaida[1].numeroAlteracao).toBe(1)
         expect(lista[0].listaArtefatoSaida[1].tipoAlteracao).toBe('A')
+        expect(lista[0].listaArtefatoSaida[1].numeroAlteracao).toBe(1)
         expect(lista[0].listaArtefatoSaida[1].nomeArtefato).toBe('foo/arquivoBar.txt')
     })
 })
