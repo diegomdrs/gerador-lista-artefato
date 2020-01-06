41a7be663be65b4417bb8c7191e4daf76df3d572
Diego Medeiros <c1282036@bb.com.br>
2020-01-06 18:19:00 -0300

    Update

--- models/arquivo.js
+++ models/arquivo.js
@@ -2,15 +2,15 @@
 const Commit = require('../models/commit')
 
 class Arquivo {
-    constructor(nomeProjeto, retorno) {
+    constructor(nomeProjeto, arquivo) {
 
         this.nomeProjeto = nomeProjeto
 
-        this.nomeArquivo = retorno.match(/[^\r\n]+/g)[1].match(/\s.+/g)[0].match(/\w.+/g)[0]
+        this.nomeArquivo = arquivo.match(/\s.+/g)[0].match(/\w.+/g)[0]
         this.nomeArquivo = this.nomeArquivo.match(/^[^\s]*/g)[0]
             .replace(/^/g, this.nomeProjeto + '/')
 
-        this.commit = new Commit(this, retorno)
+        this.commit = new Commit(this, arquivo)
     }
 }
 
