e86b521ff0413810dad4e232c9571a6b0023ac4b
Diego Medeiros <c1282036@bb.com.br>
2020-01-09 17:14:30 -0300

    Update

--- config/commander.js
+++ config/commander.js
@@ -8,7 +8,7 @@ program
     .option('-p, --projeto <type>', 'Lista de projetos Git (podem ser passados vários projetos separados por vírgula)', commaSeparatedList)
     .option('-a, --autor <type>', 'Matrícula do autor dos commits')
     .option('-t, --task <type>', 'Lista de tarefas (podem ser passadas várias tarefas separadas por vírgula)', commaSeparatedList)
-    .option('--mostrar-num-modificacao', 'Nº de modificações do artefato na tarefa ou tarefas (Opcional)', false)
+    .option('--mostrar-num-modificacao', 'Nº de modificações do artefato na tarefa ou tarefas (Opcional)')
     .option('--mostrar-deletados', 'Mostra artefatos deletados na tarefa (Opcional)')
     .option('--mostrar-renomeados', 'Mostra artefatos renomeados na tarefa (Opcional)')
     .option('-s, --server', 'Inicia a versão server e ignora os outros parâmetros')
