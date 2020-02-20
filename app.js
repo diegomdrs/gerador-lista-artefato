e870d3f987bc414908fcdb3a5f4f93916ddfa4d9
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2020-02-19 21:06:55 -0300

    Update

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ init()
 
 async function init() {
 
+    const commander = require('./config/commander')
+
+    if (commander.projeto && commander.autor && commander.task && commander.diretorio) {
+
+        require('./routes/gerador-cli')(commander)
+
+    } else if (commander.server) {
+
         const PORT = require('./lib/constants').PORT
         const HOST = require('./lib/constants').HOST
 
@@ -15,26 +23,7 @@ async function init() {
 
         open(`http://${HOST}:${PORT}`, { app: 'firefox' })
 
-    // const commander = require('./config/commander')
-
-    // if (commander.server) {
-
-    //     const PORT = require('./lib/constants').PORT
-    //     const HOST = require('./lib/constants').HOST
-
-    //     const app = require('./config/express')
-    //     const open = require('open')
-
-    //     await require('./config/http')(app)
-    //     await require('./routes/gerador-web')(app)
-
-    //     open(`http://${HOST}:${PORT}`, { app: 'firefox' })
-
-    // } else if (commander.projeto && commander.autor && commander.task && commander.diretorio) {
-
-    //     require('./routes/gerador-cli')(commander)
-    // } else {
-
-    //     commander.outputHelp()
-    // }
+    } else {
+        commander.outputHelp()
+    }
 }
--- config/commander.js
+++ config/commander.js
@@ -4,6 +4,7 @@ const package = require('../package.json')
 
 program
     .description('Comando para listar os artefatos incluídos/alterados/renomeados para geração do QAS')
+    .option('-s, --server', 'Inicia a versão server e ignora os outros parâmetros (Modo Padrão)', true)
     .option('-d, --diretorio <type>', 'Diretório raiz dos projetos Git')
     .option('-p, --projeto <type>', 'Lista de projetos Git (podem ser passados vários projetos separados por vírgula)', commaSeparatedList)
     .option('-a, --autor <type>', 'Matrícula do autor dos commits')
@@ -12,7 +13,6 @@ program
     .option('--mostrar-deletados', 'Mostra artefatos deletados na tarefa (Opcional)')
     .option('--mostrar-renomeados', 'Mostra artefatos renomeados na tarefa (Opcional)')
     .option('--mostrar-commits-locais', 'Mostra commits remotos e locais (Opcional)')
-    .option('-s, --server', 'Inicia a versão server e ignora os outros parâmetros')
     .version(package.version, '-v, --version', 'Mostra a versão do programa')
 
 function commaSeparatedList(value, dummyPrevious) {
